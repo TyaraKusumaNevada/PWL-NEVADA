88431d42e9b36cda6a8b82546823fe7e74c3c937
TyaraKusumaNevada <tyaranevada29@gmail.com>
2025-02-18 09:07:58 +0700

    modifikasi tampilan awal (js 1)

--- mencoba-laravel/resources/views/welcome.blade.php
+++ mencoba-laravel/resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <title>PWL 2024</title>
             </head>
-    <body>
+        <body>
         <h2>Pemrograman Web Lanjut 2024</h2>
         <h3>Semangat Belajar Laravel 10</h3>
     </body>
