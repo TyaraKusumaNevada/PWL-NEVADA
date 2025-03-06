4690445ccffe6ab586f645236ca48564e9241b60
TyaraKusumaNevada <tyaranevada29@gmail.com>
2025-03-06 10:58:46 +0700

    Praktikum 6 - Implementasi Eloquent ORM

--- Minggu-3/PWL_POS/routes/web.php
+++ Minggu-3/PWL_POS/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,8 @@ Route::get('/', function () {
 });
 
 //Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/user', [UserController::class, 'index']);
 
 
