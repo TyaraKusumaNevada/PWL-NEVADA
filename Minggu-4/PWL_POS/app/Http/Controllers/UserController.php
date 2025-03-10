3da7eb0daaaae88384e4cc06bd0595290ef153ba
TyaraKusumaNevada <tyaranevada29@gmail.com>
2025-03-10 19:24:01 +0700

    Praktikum 2.2 - Not Found Exceptions

--- Minggu-4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-4/PWL_POS/app/Http/Controllers/UserController.php
@@ -106,11 +106,21 @@ class UserController extends Controller
     // return view('user', ['data' => $user]);
     
     //------------PRAKTIKUM 2.1 LANGKAH 10-------------- 
-    $user = UserModel::findOr(20, ['username', 'nama'], function () {
-        abort(404);
-    });
+    // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+    //     abort(404);
+    // });
+
+    // return view('user', ['data' => $user]);
     
+
+    //------------PRAKTIKUM 2.2 LANGKAH 1-------------- 
+    // $user = UserModel::findOrFail(1);
+    // return view('user', ['data' => $user]);
+
+    //------------PRAKTIKUM 2.2 LANGKAH 3--------------
+    $user = UserModel::where('username', 'manager9')->firstOrFail();
     return view('user', ['data' => $user]);
     
+
     }
 }
