db02fc8f4001d41fb3f199300466169148b92891
TyaraKusumaNevada <tyaranevada29@gmail.com>
2025-03-16 13:12:56 +0700

    Praktikum 1 - Integrasi Laravel dengan AdminLte3

---
+++ Minggu-5/PWL_POS/resources/views/articles.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Article</title>
+</head>
+<body>
+    <h1>Ini Halaman Article</h1>
+</body>
+</html>
---
+++ Minggu-5/PWL_POS/resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+{{-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+</head>
+<body>
+    <!DOCTYPE html>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PWL 2024</title>
+    </head>
+    <body>
+        <h2>Pemrograman Web Lanjut 2024</h2>
+        <h3>Semangat Belajar Laravel 10</h3>
+    </body>
+    </html>
+</body>
+</html> --}}
+
+{{-- PRAKTIKUM 1 -LANGKAH 4 JOBSHEET 5 --}}
+@extends('layouts.app')
+
+{{-- Customize layout sections --}}
+
+@section('subtitle', 'Welcome')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Welcome')
+
+{{-- Content body: main page content --}}
+
+@section('content_body')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+{{-- Push extra CSS --}}
+
+@push('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@endpush
+
+{{-- Push extra scripts --}}
+
+@push('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@endpush
+
