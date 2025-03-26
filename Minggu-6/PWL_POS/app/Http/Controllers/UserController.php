0f8200e03c6be5a353b7eab6dd5ab10d5320a15e
TyaraKusumaNevada <tyaranevada29@gmail.com>
2025-03-26 10:01:06 +0700

    Praktikum - 1 Ajax Tambah Data (User)🍃

--- Minggu-6/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-6/PWL_POS/app/Http/Controllers/UserController.php
@@ -8,10 +8,49 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use App\Models\LevelModel;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 
 class UserController extends Controller
 {
+    //js 6 prak 1
+    public function create_ajax()
+    {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+        return view('user.create_ajax', ['level' => $level]);
+    }
+
+    public function store_ajax(Request $request) {
+        // cek apakah request berupa ajax
+        if($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama'     => 'required|string|max:100',
+                'password' => 'required|min:6'
+            ];
+
+            
+            $validator = Validator::make($request->all(), $rules);
+
+            if($validator->fails()){
+                return response()->json([
+                    'status' => false, // response status, false: error/gagal, true: berhasil
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors(), // pesan error validasi
+                ]);
+            }
+
+            UserModel::create($request->all());
+            return response()->json([
+                'status' => true,
+                'message' => 'Data user berhasil disimpan'
+            ]);
+        }
+
+        redirect('/');
+    }
+
     // jobsheet 5-baru praktikum 3 langkah 4
     // Menampilkan halaman awal user
     public function index()
@@ -38,6 +77,8 @@ class UserController extends Controller
 
         
     }
+
+
         //jobsheet 5-baru praktikum 3 langkah 7
         // Ambil data user dalam bentuk json untuk datatables
     public function list(Request $request)
