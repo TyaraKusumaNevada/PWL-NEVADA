cfc26d6b653d85e9e181c57d36dac3f29615df33
TyaraKusumaNevada <tyaranevada29@gmail.com>
2025-04-13 18:59:56 +0700

    IMPLEMENTASI JOBSHEET 5 PRAKTIKUM 3 : Implementasi jQuery Datatable di AdminLTE (user)

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -13,6 +13,10 @@ use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\StokController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\PenjualanDetailController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\AuthController; 
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ProfileController;
 
 
 /*
@@ -43,79 +47,78 @@ use App\Http\Controllers\PenjualanDetailController;
 //     return view('products');
 // });
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// js 3========================
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/barang', [BarangController::class, 'index']);
-Route::get('/supplier', [SupplierController::class, 'index']);
-Route::get('/stok', [StokController::class, 'index']);
-Route::get('/penjualan', [PenjualanController::class, 'index']);
-Route::get('/penjualanDetail', [PenjualanDetailController::class, 'index']);
-
-
-// ===============implementasi js 4================
-//================================================================Jobsheet 4===========================================================================
-//Route User
-Route::get('user/tambah', [UserController::class, 'tambah']);
-Route::post('user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
-
-//Route Level
-Route::get('level/tambah', [LevelController::class, 'tambah']);
-Route::post('level/tambah_simpan', [LevelController::class, 'tambah_simpan']);
-Route::get('level/ubah/{id}', [LevelController::class, 'ubah']);
-Route::put('level/ubah_simpan/{id}', [LevelController::class, 'ubah_simpan']);
-Route::get('level/hapus/{id}', [LevelController::class, 'hapus']);
-
-//Route Kategori
-Route::get('kategori/tambah', [KategoriController::class, 'tambah']);
-Route::post('kategori/tambah_simpan', [KategoriController::class, 'tambah_simpan']);
-Route::get('kategori/ubah/{id}', [KategoriController::class, 'ubah']);
-Route::put('kategori/ubah_simpan/{id}', [KategoriController::class, 'ubah_simpan']);
-Route::get('kategori/hapus/{id}', [KategoriController::class, 'hapus']);
-
-//Route Barang
-Route::get('barang/tambah', [BarangController::class, 'tambah']);
-Route::post('barang/tambah_simpan', [BarangController::class, 'tambah_simpan']);
-Route::get('barang/ubah/{id}', [BarangController::class, 'ubah']);
-Route::put('barang/ubah_simpan/{id}', [BarangController::class, 'ubah_simpan']);
-Route::get('barang/hapus/{id}', [BarangController::class, 'hapus']);
-
-//Route Supplier
-Route::get('supplier/tambah', [SupplierController::class, 'tambah']);
-Route::post('supplier/tambah_simpan', [SupplierController::class, 'tambah_simpan']);
-Route::get('supplier/ubah/{id}', [SupplierController::class, 'ubah']);
-Route::put('supplier/ubah_simpan/{id}', [SupplierController::class, 'ubah_simpan']);
-Route::get('supplier/hapus/{id}', [SupplierController::class, 'hapus']);
-
-//Route Stok
-Route::get('stok/tambah', [StokController::class, 'tambah']);
-Route::post('stok/tambah_simpan', [StokController::class, 'tambah_simpan']);
-Route::get('stok/ubah/{id}', [StokController::class, 'ubah']);
-Route::put('stok/ubah_simpan/{id}', [StokController::class, 'ubah_simpan']);
-Route::get('stok/hapus/{id}', [StokController::class, 'hapus']);
-
-//Route Penjualan
-Route::get('penjualan/tambah', [PenjualanController::class, 'tambah']);
-Route::post('penjualan/tambah_simpan', [PenjualanController::class, 'tambah_simpan']);
-Route::get('penjualan/ubah/{id}', [PenjualanController::class, 'ubah']);
-Route::put('penjualan/ubah_simpan/{id}', [PenjualanController::class, 'ubah_simpan']);
-Route::get('penjualan/hapus/{id}', [PenjualanController::class, 'hapus']);
-
-//Route Penjualan Detail
-Route::get('penjualan-detail/tambah', [PenjualanDetailController::class, 'tambah']);
-Route::post('penjualan-detail/tambah_simpan', [PenjualanDetailController::class, 'tambah_simpan']);
-Route::get('penjualan-detail/ubah/{id}', [PenjualanDetailController::class, 'ubah']);
-Route::put('penjualan-detail/ubah_simpan/{id}', [PenjualanDetailController::class, 'ubah_simpan']);
-Route::get('penjualan-detail/hapus/{id}', [PenjualanDetailController::class, 'hapus']);
 
 //===========Jobsheet 5==============================
 Route::get('/', [WelcomeController::class, 'index']);
+
+//route usercontroller
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']); // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
+
+//route level
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [LevelController::class, 'index']); // Menampilkan daftar level
+    Route::post('/list', [LevelController::class, 'list']); // Menampilkan data level dalam JSON untuk DataTables
+    Route::get('/create', [LevelController::class, 'create']); // Menampilkan halaman form tambah level
+    Route::post('/', [LevelController::class, 'store']); // Menyimpan data level baru
+    Route::get('/{id}', [LevelController::class, 'show']); // Menampilkan detail level
+    Route::get('/{id}/edit', [LevelController::class, 'edit']); // Menampilkan halaman form edit level
+    Route::put('/{id}', [LevelController::class, 'update']); // Menyimpan perubahan data level
+    Route::delete('/{id}', [LevelController::class, 'destroy']); // Menghapus data level
+});
+
+//route kategori
+Route::group(['prefix' => 'kategori'], function () {
+    Route::get('/', [KategoriController::class, 'index']);// menampilkan halaman awal kategori
+    Route::post('/list', [KategoriController::class, 'list']);// menampilkan data kategori dalam bentuk json untuk datatables
+    Route::get('/create', [KategoriController::class, 'create']);// menampilkan halaman form tambah kategori
+    Route::post('/', [KategoriController::class, 'store']); // menyimpan data kategori baru
+    Route::get('/{id}', [KategoriController::class, 'show']); // menampilkan detail kategori
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']); // menampilkan halaman form edit kategori
+    Route::put('/{id}', [KategoriController::class, 'update']);// menyimpan perubahan data kategori
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);// menghapus data kategori
+});
+
+//route stok 
+Route::group(['prefix' => 'stok'], function () {
+    Route::get('/', [StokController::class, 'index']);// menampilkan halaman awal stok
+    Route::post('/list', [StokController::class, 'list']);  // menampilkan data stok dalam bentuk json untuk datatables
+    Route::get('/create', [StokController::class, 'create']); // menampilkan halaman form tambah stok
+    Route::post('/', [StokController::class, 'store']); // menyimpan data stok baru
+    Route::get('/{id}', [StokController::class, 'show']); // menampilkan detail stok
+    Route::get('/{id}/edit', [StokController::class, 'edit']); // menampilkan halaman form edit stok
+    Route::put('/{id}', [StokController::class, 'update']); // menyimpan perubahan data stok
+    Route::delete('/{id}', [StokController::class, 'destroy']); // menghapus data stok
+});
+
+//route barang 
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']); // menampilkan halaman awal barang
+    Route::post('/list', [BarangController::class, 'list']); // menampilkan data barang dalam bentuk json untuk datatables
+    Route::get('/create', [BarangController::class, 'create']); // menampilkan halaman form tambah barang
+    Route::post('/', [BarangController::class, 'store']); // menyimpan data barang baru
+    Route::get('/{id}', [BarangController::class, 'show']); // menampilkan detail barang
+    Route::get('/{id}/edit', [BarangController::class, 'edit']); // menampilkan halaman form edit barang
+    Route::put('/{id}', [BarangController::class, 'update']);  // menyimpan perubahan data barang
+    Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data barang
+});
+
+//route supp
+Route::group(['prefix' => 'supplier'], function () {
+    Route::get('/', [SupplierController::class, 'index']); // Menampilkan halaman awal supplier
+    Route::post('/list', [SupplierController::class, 'list']); // Menampilkan data supplier dalam bentuk JSON untuk datatables
+    Route::get('/create', [SupplierController::class, 'create']);  // Menampilkan halaman form tambah supplier
+    Route::post('/', [SupplierController::class, 'store']); // Menyimpan data supplier baru
+    Route::get('/{id}', [SupplierController::class, 'show']); // Menampilkan detail supplier
+    Route::get('/{id}/edit', [SupplierController::class, 'edit']); // Menampilkan halaman form edit supplier
+    Route::put('/{id}', [SupplierController::class, 'update']);// Menyimpan perubahan data supplier
+    Route::delete('/{id}', [SupplierController::class, 'destroy']); // Menghapus data supplier
+});
