f773b9c08a718a6e15dd8c941e126403cd86547a
TyaraKusumaNevada <tyaranevada29@gmail.com>
2025-04-19 08:50:43 +0700

    IMPLEMENTASI : Perbaikan Modal Ajax Show Data (penjualan) 🍀

--- POS/resources/views/penjualan/show_ajax.blade.php
+++ POS/resources/views/penjualan/show_ajax.blade.php
@@ -1,55 +1,74 @@
 @empty($penjualan)
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <div class="modal-header bg-danger">
+                <h5 class="modal-title">Kesalahan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Tutup">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <div class="alert alert-danger">
-                    <h5><i class="icon fas fa-ban"></i> Kesalahan!</h5>
-                    Data penjualan tidak ditemukan.
+                    Data yang Anda cari tidak ditemukan.
                 </div>
-                <a href="{{ url('/penjualan') }}" class="btn btn-warning">Kembali</a>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn btn-secondary">Tutup</button>
             </div>
         </div>
     </div>
 @else
-    <div id="modal-master" class="modal-dialog" role="document">
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header bg-info">
-                <h5 class="modal-title" id="exampleModalLabel">Detail Data Penjualan</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <h5 class="modal-title">Detail Data Penjualan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Tutup">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <table class="table table-sm">
-                    <tbody style="font-size: 0.95rem;">
-                        <tr>
-                            <th class="text-muted" style="width: 30%;">ID</th>
-                            <td>{{ $penjualan->penjualan_id }}</td>
-                        </tr>
-                        <tr>
-                            <th class="text-muted">Kode Penjualan</th>
-                            <td>{{ $penjualan->penjualan_kode }}</td>
-                        </tr>
-                        <tr>
-                            <th class="text-muted">Tanggal</th>
-                            <td>{{ $penjualan->penjualan_tanggal }}</td>
-                        </tr>
-                        <tr>
-                            <th class="text-muted">Pembeli</th>
-                            <td>{{ $penjualan->pembeli }}</td>
-                        </tr>
-                        <tr>
-                            <th class="text-muted">Kasir</th>
-                            <td>{{ $penjualan->user->username ?? '-' }}</td>
-                        </tr>
-                        <tr>
+                <table class="table table-bordered table-striped">
+                    <tr>
+                        <th style="width: 30%;">ID</th>
+                        <td>{{ $penjualan->penjualan_id }}</td>
+                    </tr>
+                    <tr>
+                        <th>Kode Penjualan</th>
+                        <td>{{ $penjualan->penjualan_kode }}</td>
+                    </tr>
+                    <tr>
+                        <th>Pembeli</th>
+                        <td>{{ $penjualan->pembeli }}</td>
+                    </tr>
+                    <tr>
+                        <th>Tanggal</th>
+                        <td>{{ $penjualan->penjualan_tanggal }}</td>
+                    </tr>
+                    <tr>
+                        <th>User</th>
+                        <td>{{ $penjualan->user->username ?? '-' }}</td>
+                    </tr>
+                </table>
 
+                <h5 class="mt-4">Detail Penjualan</h5>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Barang</th>
+                            <th>Harga</th>
+                            <th>Jumlah</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($penjualan->penjualanDetail as $key => $detail)
+                        <tr>
+                            <td>{{ $key + 1 }}</td>
+                            <td>{{ $detail->barang->barang_nama ?? $detail->barang_id }}</td>
+                            <td>{{ number_format($detail->harga, 0, ',', '.') }}</td>
+                            <td>{{ $detail->jumlah }}</td>
+                        </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
