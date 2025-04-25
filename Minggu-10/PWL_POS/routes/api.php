9b354d32921f67ed363bba260efc7d03c13ce9bf
TyaraKusumaNevada <tyaranevada29@gmail.com>
2025-04-25 12:10:58 +0700

    Jobsheet 10: Implementasi RESTful API CRUD m_user, m_kategori, m_barang   (Tugas) 🍁

--- Minggu-10/PWL_POS/routes/api.php
+++ Minggu-10/PWL_POS/routes/api.php
@@ -23,13 +23,33 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
-
-
 Route::get('levels', [LevelController::class, 'index']);
 Route::post('levels', [LevelController::class, 'store']);
 Route::get('levels/{level}', [LevelController::class, 'show']);
 Route::put('levels/{level}', [LevelController::class, 'update']);
 Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+use App\Http\Controllers\Api\UserController;
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{user}', [UserController::class, 'show']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
+
+use App\Http\Controllers\Api\KategoriController;
+Route::get('kategoris', [KategoriController::class, 'index']);
+Route::post('kategoris', [KategoriController::class, 'store']);
+Route::get('kategoris/{kategori}', [KategoriController::class, 'show']);
+Route::put('kategoris/{kategori}', [KategoriController::class, 'update']);
+Route::delete('kategoris/{kategori}', [KategoriController::class, 'destroy']);
+
+
+use App\Http\Controllers\Api\BarangController;
+Route::get('barangs', [BarangController::class, 'index']);
+Route::post('barangs', [BarangController::class, 'store']);
+Route::get('barangs/{barang}', [BarangController::class, 'show']);
+Route::put('barangs/{barang}', [BarangController::class, 'update']);
+Route::delete('barangs/{barang}', [BarangController::class, 'destroy']);
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
