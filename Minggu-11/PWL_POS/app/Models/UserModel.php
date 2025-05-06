16cc47f4ba37c4aaf011c91f97cd6c6ae522bd24
TyaraKusumaNevada <tyaranevada29@gmail.com>
2025-05-06 07:59:09 +0700

    Praktikum 1 – Implementasi Eloquent Accessor🍁

--- Minggu-11/PWL_POS/app/Models/UserModel.php
+++ Minggu-11/PWL_POS/app/Models/UserModel.php
@@ -1,73 +1,115 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Tymon\JWTAuth\Contracts\JWTSubject;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
-
-
-class UserModel extends Authenticatable implements JWTSubject
-{
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
-    use HasFactory;
-    protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan di model ini
-    protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
-
-    //=======Jaobsheet 4 Prak 1=======
-
-    protected $fillable = ['username', 'nama', 'password', 'level_id'];
-
-    protected $hidden = ['password']; // jangan di tampilkan saat select
-
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+<?php 
+ 
+namespace App\Models; 
+ 
+use Illuminate\Database\Eloquent\Model; 
+use Tymon\JWTAuth\Contracts\JWTSubject; 
+use Illuminate\Database\Eloquent\Casts\Attribute; 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
-//    ===Jobsheet 4 Prakt 2.7====
+class UserModel extends Authenticatable implements JWTSubject 
+{ 
+ 
+    public function getJWTIdentifier(){ 
+        return $this->getKey(); 
+    } 
+ 
+    public function getJWTCustomClaims(){ 
+        return []; 
+    } 
+ 
+    protected $table = 'm_user'; 
+    protected $primaryKey = 'user_id'; 
+ 
+    protected $fillable = [ 
+        'username', 
+        'nama', 
+        'password', 
+        'level_id', 
+        'image'//tambahan 
+    ]; 
+ 
+    public function level() 
+    { 
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id'); 
+    } 
+    protected function image(): Attribute 
+    { 
+        return Attribute::make( 
+            get: fn ($image) => url('/storage/posts/' . $image), 
+        ); 
+    } 
+} 
+
+// namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Relations\BelongsTo;
+// use Tymon\JWTAuth\Contracts\JWTSubject;
+// use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+
+
+// class UserModel extends Authenticatable implements JWTSubject
+// {
+//     public function getJWTIdentifier()
+//     {
+//         return $this->getKey();
+//     }
+
+//     public function getJWTCustomClaims()
+//     {
+//         return [];
+//     }
+//     use HasFactory;
+//     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan di model ini
+//     protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
+
+//     //=======Jaobsheet 4 Prak 1=======
+
+//     protected $fillable = ['username', 'nama', 'password', 'level_id'];
+
+//     protected $hidden = ['password']; // jangan di tampilkan saat select
+
+//     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+
+
+// //    ===Jobsheet 4 Prakt 2.7====
    
-   public function level(): BelongsTo //Menunjukkan bahwa setiap user memiliki relasi belongsTo dengan tabel LevelModel, dihubungkan melalui level_id.
-   {
-       return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-   }
+//    public function level(): BelongsTo //Menunjukkan bahwa setiap user memiliki relasi belongsTo dengan tabel LevelModel, dihubungkan melalui level_id.
+//    {
+//        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//    }
 
-   public function stok(): HasMany {
-       return $this->hasMany(StokModel::class, 'user_id', 'user_id');
-   }
+//    public function stok(): HasMany {
+//        return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+//    }
 
-   public function penjualan(): HasMany {
-       return $this->hasMany(PenjualanModel::class, 'user_id', 'user_id');
-   }
+//    public function penjualan(): HasMany {
+//        return $this->hasMany(PenjualanModel::class, 'user_id', 'user_id');
+//    }
 
-    /**
-     * Mendapatkan nama role
-     */
-    public function getRoleName(): string
-    {
-        return $this->level->level_nama;
-    }
+//     /**
+//      * Mendapatkan nama role
+//      */
+//     public function getRoleName(): string
+//     {
+//         return $this->level->level_nama;
+//     }
 
-    /**
-     * Cek apakah user memiliki role tertentu
-     */
-    public function hasRole($role): bool
-    {
-        return $this->level->level_kode == $role;
-    }
+//     /**
+//      * Cek apakah user memiliki role tertentu
+//      */
+//     public function hasRole($role): bool
+//     {
+//         return $this->level->level_kode == $role;
+//     }
 
-    // mendapat kode role
-    public function getRole()
-    {
-        return $this->level->level_kode;
-    }
+//     // mendapat kode role
+//     public function getRole()
+//     {
+//         return $this->level->level_kode;
+//     }
 
-}
+// } -->
--- Minggu-11/PWL_POS/routes/api.php
+++ Minggu-11/PWL_POS/routes/api.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\Api\LevelController;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/register1', App\Http\Controllers\Api\RegisterController::class)->name('register1');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
